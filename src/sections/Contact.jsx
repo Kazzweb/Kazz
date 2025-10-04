6a82c8406ae7db261edc7584fa7db2038ce48628
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-10-04 19:39:20 +0530

    complete

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,88 @@
+import { socials } from '../constants';
+import AnimatedHeaderSection from '../components/AnimatedHeaderSection';
+import Marquee from '../components/Marquee';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+const Contact = () => {
+  const items = [
+    'just imagine, I code',
+    'just imagine, I code',
+    'just imagine, I code',
+    'just imagine, I code',
+  ];
+  const text = `Got a question, how or project Idea?
+  I'd love to hear from you and discuss further!`;
+  useGSAP(() => {
+    gsap.from('.social-link', {
+      y: 100,
+      opacity: 0,
+      delay: 0.5,
+      duration: 1,
+      stagger: 0.3,
+      ease: 'back.out',
+      scrollTrigger: {
+        trigger: '.social-link',
+      },
+    });
+  });
+  return (
+    <section
+      id='contact'
+      className='flex flex-col justify-between min-h-screen bg-black'
+    >
+      <div>
+        <AnimatedHeaderSection
+          subtitle={'You Dream It, I Code it'}
+          title={'Contact'}
+          text={text}
+          textColor={'text-white'}
+          withSrollTriggerr={true}
+        />
+        <div className='flex px-10 font-light text-white uppercase lg:text-[32px] text-[26px] leading-none mb-10'>
+          <div className='flex flex-col w-full gap-10'>
+            <div className='social-link'>
+              <h2>E-mail</h2>
+              <div className='w-full h-px my-2 bg-white opacity-30' />
+              <p className='text-xl tracking-wider lowercase md:text-2xl lg:text-3xl'>
+                JohnDoe@gmail.com
+              </p>
+            </div>
+            <div className='social-link'>
+              <h2>Phone</h2>
+              <div className='w-full h-px my-2 bg-white opacity-30' />
+              <p className='text-xl tracking-wider lowercase md:text-2xl lg:text-3xl'>
+                +91 9221968131
+              </p>
+            </div>
+            <div className='social-link'>
+              <h2>Social Media</h2>
+              <div className='w-full h-px my-2 bg-white opacity-30' />
+              <div className='flex flex-wrap gap-2'>
+                {socials.map((social, index) => {
+                  return (
+                    <a
+                      href={social.href}
+                      className='text-xs leading-loose tracking-widest uppercase md:text-sm hover:text-white/80 transition-colors duration-200'
+                      key={index}
+                    >
+                      {''}
+                      {social.name}
+                      {''}
+                    </a>
+                  );
+                })}
+              </div>
+              <p className='text-xl tracking-wider lowercase md:text-2xl lg:text-3xl'>
+                +91 9221968131
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Marquee items={items} className='text-white bg-transparent' />
+    </section>
+  );
+};
+
+export default Contact;
--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -1,10 +1,99 @@
 import { Icon } from '@iconify/react';
 import AnimatedHeaderSection from '../components/AnimatedHeaderSection';
 import { projects } from '../constants';
+import { useRef, useState } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
 
 const Work = () => {
   const text = `Featured projects that have been meticulously
   crafed with passion to drive results and impact.`;
+
+  const previewRef = useRef(null);
+
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const mouse = useRef({ x: 0, y: 0 });
+  const moveX = useRef(null);
+  const moveY = useRef(null);
+  const overlayRefs = useRef([]);
+  useGSAP(() => {
+    moveX.current = gsap.quickTo(previewRef.current, 'x', {
+      duration: 1.5,
+      ease: 'power3.out',
+    });
+    moveY.current = gsap.quickTo(previewRef.current, 'y', {
+      duration: 2,
+      ease: 'power3.out',
+    });
+
+    gsap.from('#project', {
+      y: 100,
+      opacity: 0,
+      delay: 0.5,
+      duration: 1,
+      stagger: 0.3,
+      ease: 'back.out',
+      scrollTrigger: {
+        trigger: '#project',
+      },
+    });
+  });
+
+  const handleMouseEnter = (index) => {
+    if (window.innerWidth < 768) return;
+    setCurrentIndex(index);
+    const el = overlayRefs.current[index];
+    if (!el) return;
+
+    gsap.killTweensOf(el);
+    gsap.fromTo(
+      el,
+      {
+        clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)',
+      },
+      {
+        clipPath: 'polygon(0 100%, 100% 100%, 100% 0, 0 0)',
+        duration: 0.15,
+        ease: 'power2.out',
+      }
+    );
+
+    gsap.to(previewRef.current, {
+      opacity: 1,
+      scale: 1,
+      duration: 0.3,
+      ease: 'power2.out',
+    });
+  };
+  const handleMouseLeave = (index) => {
+    if (window.innerWidth < 768) return;
+    setCurrentIndex(null);
+    const el = overlayRefs.current[index];
+    if (!el) return;
+
+    gsap.killTweensOf(el);
+    gsap.to(el, {
+      clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)',
+      duration: 0.2,
+      ease: 'power2.in',
+    });
+    gsap.to(previewRef.current, {
+      opacity: 0,
+      scale: 0.95,
+      duration: 0.3,
+      ease: 'power2.out',
+    });
+  };
+  const handleMouseMove = (e) => {
+    if (window.innerWidth < 768) return;
+    mouse.current.x = e.clientX + 24;
+    mouse.current.y = e.clientY + 24;
+    moveX.current(mouse.current.x);
+    moveY.current(mouse.current.y);
+  };
   return (
     <section id='work' className='min-h-screen flex flex-col'>
       <AnimatedHeaderSection
@@ -14,14 +103,26 @@ const Work = () => {
         textColor={'text-black'}
         withSrollTriggerr={true}
       />
-      <div className='relative flex flex-col font-light'>
+      <div
+        className='relative flex flex-col font-light'
+        onMouseMove={handleMouseMove}
+      >
         {projects.map((project, index) => {
           return (
             <div
               key={project.id}
               id='project'
               className='relative flex flex-col gap-1 py-5 cursor-pointer group md:gap-0'
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
             >
+              {/* ovelay */}
+              <div
+                className='absolute inset-0 hidden lg:block duration-200 bg-black -z-10 clip-path'
+                ref={(el) => {
+                  overlayRefs.current[index] = el;
+                }}
+              />
               {/* title */}
               <div className='flex justify-between px-10 text-black transition-all duration-500 md:group-hover:text-white md:group-hover:px-12'>
                 <h2 className='lg:text-[32px] text-[26px] leading-none'>
@@ -33,10 +134,49 @@ const Work = () => {
                 />
               </div>
               {/* divider */}
-              <div className='w-full' />
+              <div className='w-full h-0.5 bg-black opacity-80' />
+              {/* framework */}
+              <div className='flex px-10 text-xs leading-loose uppercase transition-all duration-500 md:text-sm gap-x-5 md:group-hover:px-12'>
+                {project.frameworks.map((framework) => {
+                  return (
+                    <p
+                      key={framework.id}
+                      className='text-black transition-colors duration-500 md:group-hover:text-white'
+                    >
+                      {framework.name}
+                    </p>
+                  );
+                })}
+              </div>
+              {/* mobile preview images */}
+              <div className='relative flex items-center justify-center px-10 md:hidden h-[400px]'>
+                <img
+                  src={project.bgImage}
+                  alt={`${project.name}-bg-image`}
+                  className='object-cover w-full h-full rounded-md brightness-50'
+                />
+                <img
+                  src={project.image}
+                  alt={`${project.name}-image`}
+                  className='absolute bg-center px-14 rounded-xl'
+                />
+              </div>
             </div>
           );
         })}
+        {/* desktop floating preview image */}
+        <div
+          ref={previewRef}
+          className='fixed -top-2/6 left-0 z-50 overflow-hidden border-8 border-black pointer-events-none w-[700px] 2xl:w-[960px] md:block hidden opacity-0'
+        >
+          {currentIndex !== null && (
+            <img
+              src={projects[currentIndex].image}
+              alt={`perview`}
+              className='object-cover w-full h-full'
+            />
+          )}
+        </div>
       </div>
     </section>
   );
