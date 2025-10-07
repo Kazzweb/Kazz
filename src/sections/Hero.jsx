3931acf9450d9ea1f52de187038a85348ef1b1e9
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-10-07 16:42:25 +0530

    testing

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -80,7 +80,9 @@ const Hero = () => {
         <Canvas
           shadows
           camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
-          performance={{ min: 0.3 }}
+          gl={{ antialias: !isMobile }}
+          dpr={isMobile ? [1, 1.4] : [1, 2]}
+          frameloop={isMobile ? 'demand' : 'always'}
         >
           <ambientLight intensity={0.5} />
           <Environment resolution={isMobile ? 64 : 256}>
